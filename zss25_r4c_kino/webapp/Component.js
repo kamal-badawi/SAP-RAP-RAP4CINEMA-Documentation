98caa8cb57998ebc566a08a93dc31a9a6ec48eaa
kamal-badawi <kamal.projekte@gmail.com>
2025-10-18 01:14:30 +0200

    CSS in Fiori integriert

--- zss25_r4c_kino/webapp/Component.js
+++ zss25_r4c_kino/webapp/Component.js
@@ -1,12 +1,11 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
-        "use strict";
-
-        return Component.extend("com.zss25.learnr4c.kinoverwaltung.zss25r4ckino.Component", {
-            metadata: {
-                manifest: "json"
-            }
-        });
-    }
-);
+sap.ui.define(["sap/fe/core/AppComponent"], function(Component) {
+    "use strict";
+    return Component.extend("com.zss25.learnr4c.kinoverwaltung.zss25r4ckino.Component", {
+        metadata: { manifest: "json" },
+        init: function() {
+            Component.prototype.init.apply(this, arguments);
+            // CSS einbinden
+            jQuery.sap.includeStyleSheet(jQuery.sap.getModulePath("com.zss25.learnr4c.kinoverwaltung.zss25r4ckino.css.style"));
+        }
+    });
+});
